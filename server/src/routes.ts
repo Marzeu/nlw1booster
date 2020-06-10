77f26bd7b1a44cef3b861ee74f71a8d850114df2
marzeu <marzahell@hotmail.com>
2020-06-10 01:00:57 -0300

    listagem de items

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'Hell World' });
+routes.get('/items', async (req, res) => {
+    const items = await knex('items').select('*');
+
+    const serializedItens = items.map(item => {
+        return {
+            title: item.name,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        };
+    });
+
+    return res.json(serializedItens);
 });
 
 export default routes;
