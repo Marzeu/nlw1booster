390c126ba624aeeede1784a7a2a1be28b01d6fe5
marzeu <marzahell@hotmail.com>
2020-06-10 03:20:40 -0300

    rota de pontos de coletas filtrados

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,7 +8,9 @@ const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
+
 routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
 export default routes;
